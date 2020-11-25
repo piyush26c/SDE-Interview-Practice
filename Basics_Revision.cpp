3f2aa5873a5249af984d3403cfdd2e818bf3ea9a
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2020-11-25 09:53:40 +0530

    allPermutation function implemented.
    print all posiible number of permutations of a given string.

--- Basics_Revision.cpp
+++ Basics_Revision.cpp
@@ -17,3 +17,19 @@ swap (x, y) {
 	y = x - y;
 	x = x - y;
 }
+//print all permutation
+void allPermutation (string &inputString_, int leftIndx_, int rightIndx_) {
+	//base case
+	if (leftIndx_ == rightIndx_) {
+		cout << inputString_ << "\n";
+	} else {
+		for (int indx = leftIndx_; indx <= rightIndx_; indx++) {
+			swap(inpuptString_[leftIndx_], inputString_[indx]);
+			allPermutation(inputString_, indx + 1, rightIndx_);
+			swap(inputString_[leftIndx_], inputString_[indx]);
+		}
+	}
+} //time complexity : O(n * n!)
+
+
+
