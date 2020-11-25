dcbdaae7a308bad6df0330d1f289e4268d6615ed
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2020-11-25 09:28:59 +0530

    basics added.

---
+++ Basics_Revision.cpp
@@ -0,0 +1,19 @@
+//new delete 
+	- int *ip = new int;
+	- int *jp = new int [10]; //array
+	- delete []jp;;
+	- delete ip;
+//exception catching
+	- catch (char* param_) {}
+	- catch (...) {}
+//algorithm specification
+	- Input:
+	- Output:
+	- Definiteness:
+	- Effectiveness:
+//swaping without temp: 
+swap (x, y) {
+	x = x + y;
+	y = x - y;
+	x = x - y;
+}
