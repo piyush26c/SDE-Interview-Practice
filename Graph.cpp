4c8329f5192ac116f5d1384f626f7be11d219cc6
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2021-03-01 09:27:40 +0530

    Create Graph.cpp

---
+++ Graph.cpp
@@ -0,0 +1,24 @@
+#######################################################################################################
+--------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
+
+1]
+Kosaraju's Strongly Connected Component Algorithm
+References for Algorithm: https://www.hackerearth.com/practice/algorithms/graphs/strongly-connected-components/tutorial/
+
+
+
+
+
+
+
+
+int main() {
+	int nov, noe; //number of vertices, number of edges
+	cin >> nov;
+	vector<vector<int> graph;
+	vector<bool> visited(nov, false);
+	
+	
+	return 0;	
+}
