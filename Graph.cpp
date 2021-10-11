c2d98c8de8bf6be62af4405f2640d700096fb9a0
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2021-10-11 20:12:53 +0530

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -16,8 +16,14 @@ References for Algorithm: https://www.hackerearth.com/practice/algorithms/graphs
 int main() {
 	int nov, noe; //number of vertices, number of edges
 	cin >> nov;
-	vector<vector<int> graph;
+	unordered_map<int, vector<int>> graph;
 	vector<bool> visited(nov, false);
+	//input edges from -> to
+	for (int i = 0; i < noe; i++) {
+		int from, to;
+		cin >> from >> to;
+		graph[from].push_back(to);		
+	}
 	
 	
 	return 0;	
