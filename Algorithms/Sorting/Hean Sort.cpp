fe71e2db800a960cd1c206b74ed8271ed1de88d5
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2021-12-12 09:04:05 +0530

    Binary heap | Heap Sort
    learned why global index of parent in binary heap array is floor((i - 1) / 2) 🍧

---
+++ Algorithms/Sorting/Hean Sort.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Video tutorial: https://youtu.be/koyuy564TZ8
+*/
+
+// Proof Link of why parent node global index is floor ((i - 1) / 2)
+// Link: https://cs.stackexchange.com/questions/130167/why-does-the-formula-floori-1-2-find-the-parent-node-in-a-binary-heap
+
