247a6123fc128874b318d5a40f3e6b714c99cc77
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2022-01-08 18:34:46 +0530

    optimized code
    optimization done!

--- Solutions/Graph Problems/snake_ladder_game_solution.cpp
+++ Solutions/Graph Problems/snake_ladder_game_solution.cpp
@@ -1,36 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int min_dice_throws(int n,vector<pair<int,int> > snakes, vector<pair<int,int> > ladders){
+/*
+	optimized version
+	| _ In previous version, we were checking whehter a block has a start to ladder | snake
+	| _ Instead we can mark those block who has start and while adding the edge we will keep eye on it ...
+*/
+
+int min_dice_throws(int n, vector<pair<int, int>> snakes, vector<pair<int, int>> ladders){
     
     int throws = 0;
     
-    vector<vector<int>> graph (n + 1);
+    vector<vector<int>> graph(n + 1);
+	
+	vector<int> board(n + 1, 0);
     
-    function<int(int)> isLadder = [&](int from_) {
-        int ans = -1;
-        
-        for (auto &x : ladders) {
-            if (x.first == from_) {
-                ans = x.second;
-                break;
-            }
-        }
-        return ans;
-    };
-    
-    function<int(int)> isSnake = [&](int from_) {
-        int ans = -1;
-        
-        for (auto &x : snakes) {
-            if (x.first == from_) {
-                ans = x.second;
-                break;
-            }
-        }
-        return ans;
-    };   
-    
+	// optimization ... 
+	for (auto &x : snakes) {
+		int from = x.first;
+		int to = x.second;
+		
+		board[x.first] = x.second - x.first;
+	}
+	
+	for (auto &x : ladders) {
+		int from = x.first;
+		int to = x.second;
+		
+		board[x.first] = x.second - x.first;
+	}
+	// ...
+	    
     // directed graph
     for (int i = 1; i <= n; i++) {
         int from = i;
@@ -38,16 +38,8 @@ int min_dice_throws(int n,vector<pair<int,int> > snakes, vector<pair<int,int> > 
         for (int j = 1; j <= 6 && (from + j <= n); j++) {
             int to = from + j;
             
-            int toL_ = isLadder(to);
-            int toS_ = isSnake(to);
-            
-            if ((toL_ == -1) && (toS_ == -1)) {
-                graph[from].push_back(to);
-            } else if (toL_ != -1) {
-                graph[from].push_back(toL_);
-            } else {
-                graph[from].push_back(toS_);
-            }
+			to += board[to];
+			graph[from].push_back(to);
         }
     }
       
