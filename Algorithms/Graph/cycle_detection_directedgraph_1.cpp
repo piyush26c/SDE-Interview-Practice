3f27588cda25da9b2f1c83eb027d346a1d5e474a
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2022-01-14 09:44:26 +0530

    added code for multiple compnnent

--- Algorithms/Graph/cycle_detection_directedgraph_1.cpp
+++ Algorithms/Graph/cycle_detection_directedgraph_1.cpp
@@ -13,8 +13,10 @@ class Graph {
 	void addEdge(int from, int to) {
 		l[from].push_back(to);				
 	}
-	
+
 	bool dfs(int node, vector<bool> &visited, vector<bool> &isInCurrentPath) {
+		// return true if backedge is found, else false
+		
         visited[node] = true;
         isInCurrentPath[node] = true;
         
@@ -35,21 +37,34 @@ class Graph {
 	
 	bool containsCycle() {
 		vector<bool> visited(n, false);
-        vector<bool> isInCurrentPath(n, false);
-        bool ans = dfs(0, visited, isInCurrentPath);
-        return ans;
+        vector<bool> isInCurrentPath(n, false);	// isInCurrentPath track the nodes which are in current path		
+		bool ans = false;
+		
+		// this loop is made because to check for other components (when graph is not a single component)
+		for (int i = 0; i < n; i++) {
+			int source = i;
+			if (!visited[source]) {
+				ans = dfs(source, visited, isInCurrentPath);
+				if (ans) break;
+			}
+		}
+		return ans;
 	}		
 };
 
 int main(){
-	Graph g(6);
+	Graph g(10);
 	g.addEdge(0,1);
 	g.addEdge(1,2);
 	g.addEdge(2,3);
-	g.addEdge(3,0);
+	g.addEdge(0,3);
 	g.addEdge(0,4);
 	g.addEdge(0,5);
 	g.addEdge(4,5);
+    g.addEdge(6,7);
+    g.addEdge(7,8);
+    g.addEdge(8,9);
+    g.addEdge(9,6);
 
 	cout << "Directed Graph contains cycle ?\nAns. " << g.containsCycle() << "\n";
 	return 0;
