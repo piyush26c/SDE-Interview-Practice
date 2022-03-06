78a8bf46fbe6095610f376d86df532255e10bcd3
Piyush Rajendra Chaudhari <piyushrc26@gmail.com>
2022-03-06 15:05:08 +0530

    condensedGraph code updated & test case added

--- Algorithms/Graph/Kosaraju Algorithm Strong connected component.cpp
+++ Algorithms/Graph/Kosaraju Algorithm Strong connected component.cpp
@@ -23,7 +23,8 @@ class Graph {
 	vector<int> nodeToComponent;	// to store component number of each node(number) either way we can store in a way of adjacency list!
 	vector<vector<int>> condensedGraph;
 	vector<int> rootNodes; // the list of all root nodes (one per component) in the condensation graph.
-
+	vector<int> components;
+	vector<int> rootOfNode;
 	int V;
 
 	public:
@@ -35,6 +36,7 @@ class Graph {
 		noOfComponents = 0;
 		nodeToComponent.resize(V + 1);
 		condensedGraph.resize(V + 1);
+		rootOfNode.resize(V + 1, 0);
 	}
 
 	void add_edge(int node1, int node2) {
@@ -67,12 +69,12 @@ class Graph {
 		return;
 	}
 
-	// traversing(DFS) on reverse graph, particularly on first sink(SCC) in topological order 
+	// traversing(DFS) on reverse graph, particularly on first sink(SCC) in topological order
 	void dfs2(int current_node, int noOfComponents) {
 		visited[current_node] = true;
 		nodeToComponent[current_node] = noOfComponents;
-		
-		
+		components.push_back(current_node);
+
 		for (auto child : rgraph[current_node]) {
 			if (!visited[child]) {
 				dfs2(child, noOfComponents);
@@ -84,13 +86,17 @@ class Graph {
 	void kosarajuAlgo() {
 		fill(visited.begin(), visited.end(), false);
 
-		for (int i = 1; i <= V; i++) {
-			if (!visited[i]) {
-				dfs2(i, ++noOfComponents);
-				
+		for (auto node : order) {
+			if (!visited[node]) {
+				dfs2(node, ++noOfComponents);
+
 				// below provision is to keep track of nodes that will be there in condensedGraph
 				int root = components.front();
+				for (auto nd : components) {
+					rootOfNode[nd] = root;
+				}
 				rootNodes.push_back(root);
+				components.clear();
 			}
 		}
 
@@ -98,54 +104,64 @@ class Graph {
 	}
 
 	void printNodeToComponent() {
-		cout << "\nNode  ->  Component(Number): \n";
+		cout << "\nNode	 ->	 Component(Number): \n";
 		for (int i = 1; i <= V; i++) {
-			cout << i << "  ->  " << nodeToComponent[i] << "\n";
+			cout << i << "	->	" << nodeToComponent[i] << "\n";
 		}
 		return;
 	}
-	
+
 	void createCondensedGraph() {
 		for (int from = 1; from <= V; from++) {
 			for (auto to : graph[from]) {
-				int root_from = components[from];
-				int root_to = components[to];
-				
-				if (root_from ! root_to) {
-					condensedGraph[from].push_back(to);
+				int root_from = rootOfNode[from];
+				int root_to = rootOfNode[to];
+
+				if (root_from != root_to) {
+					condensedGraph[root_from].push_back(root_to);
 				}
 			}
 		}
 		return;
 	}
-	
+
 	void printCondensedGraph() {
+		cout << "\nCondensed Graph: \n";
+
 		for (int from = 1; from <= V; from++) {
 			if (condensedGraph[from].size() == 0) continue;
-			
+
 			cout << from << "  ->  ";
 			for (auto to : condensedGraph[from]) {
 				cout << to << " ";
 			}
 			cout << "\n";
 		}
+		return;
 	}
-	
+
 };
 
 int main() {
 
-	Graph g(4);
+	Graph g(9);
 	g.add_edge(1, 2);
 	g.add_edge(2, 3);
 	g.add_edge(3, 1);
 	g.add_edge(3, 4);
-	g.add_edge(4, 3);
+	g.add_edge(4, 5);
+	g.add_edge(5, 6);
+	g.add_edge(6, 4);
+	g.add_edge(7, 2);
+	g.add_edge(7, 8);
+	g.add_edge(8, 9);
+	g.add_edge(9, 7);
 
 	g.getTopologicalOrder();
 	g.kosarajuAlgo();
 	g.printNodeToComponent();
 	g.createCondensedGraph();
+	g.printCondensedGraph();
 
 	return 0;
 }
@@ -170,4 +186,22 @@ int main() {
 	g.add_edge(2, 3);
 	g.add_edge(3, 1);
 	g.add_edge(3, 4);
+*/
+
+/*
+	Test Case - 3
+
+	Graph g(9);
+	g.add_edge(1, 2);
+	g.add_edge(2, 3);
+	g.add_edge(3, 1);
+	g.add_edge(3, 4);
+	g.add_edge(4, 5);
+	g.add_edge(5, 6);
+	g.add_edge(6, 4);
+	g.add_edge(7, 2);
+	g.add_edge(7, 8);
+	g.add_edge(8, 9);
+	g.add_edge(9, 7);
+
 */
